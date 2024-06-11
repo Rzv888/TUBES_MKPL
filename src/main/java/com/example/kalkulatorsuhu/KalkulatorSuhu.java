e8479adcf1ece308b477f156a60024f332c6d154
aryaashari <aryaashari100@gmail.com>
2024-06-11 14:44:45 +0700

    add unit test and fix some functions

--- src/main/java/com/example/kalkulatorsuhu/KalkulatorSuhu.java
+++ src/main/java/com/example/kalkulatorsuhu/KalkulatorSuhu.java
@@ -25,26 +25,23 @@ public class KalkulatorSuhu {
 
     }
     int getCelcius(){
-        if (this.jenis_satuan_awal == "Celcius"){
-
+        if (this.jenis_satuan_awal.equals("Celcius")) {
             return this.suhu;
-        }else if(this.jenis_satuan_awal == "Fahrenheit"){
-            return (5/9) * this.suhu - 32;
-        }else if (this.jenis_satuan_awal == "Kelvin"){
-            
-            return  this.suhu - 273;
+        } else if (this.jenis_satuan_awal.equals("Fahrenheit")) {
+            return (int) ((this.suhu - 32) * (5.0 / 9.0));
+        } else if (this.jenis_satuan_awal.equals("Kelvin")) {
+            return this.suhu - 273;
         }
         return 0;
 
     }
     int getFahrenheit(){
-        if (this.jenis_satuan_awal == "Celcius"){
-
-            return (9/5)* this. suhu + 32;
-        }else if(this.jenis_satuan_awal == "Fahrenheit"){
+        if (this.jenis_satuan_awal.equals("Celcius")) {
+            return (int) ((9.0/5.0) * this.suhu + 32);
+        } else if (this.jenis_satuan_awal.equals("Fahrenheit")) {
             return this.suhu;
-        }else if (this.jenis_satuan_awal == "Kelvin"){
-            return (9/5)*(this.suhu - 273) + 273;
+        } else if (this.jenis_satuan_awal.equals("Kelvin")) {
+            return (int) ((9.0/5.0) * (this.suhu - 273) + 32);
         }
         return 0;
     }
