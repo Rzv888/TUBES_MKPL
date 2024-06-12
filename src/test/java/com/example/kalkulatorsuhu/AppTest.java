f68335efc79d36bec871ac9cbb73b1a98048d23e
Arya Ashari <61337332+Aryaashari@users.noreply.github.com>
2024-06-12 08:54:42 +0700

    Update AppTest.java

--- src/test/java/com/example/kalkulatorsuhu/AppTest.java
+++ src/test/java/com/example/kalkulatorsuhu/AppTest.java
@@ -7,19 +7,19 @@ public class AppTest {
     @Test
     public void testGetKelvinFromCelcius() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
-        assertEquals(273, kalkulator.getKelvin());
+        assertEquals(0, kalkulator.getKelvin());
     }
 
     @Test
     public void testGetKelvinFromKelvin() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
-        assertEquals(273, kalkulator.getKelvin());
+        assertEquals(0, kalkulator.getKelvin());
     }
 
     @Test
     public void testGetKelvinFromFahrenheit() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(32, "Fahrenheit");
-        assertEquals(273, kalkulator.getKelvin());
+        assertEquals(0, kalkulator.getKelvin());
     }
 
     @Test
