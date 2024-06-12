a9875a6945f5008c922987203671dfe2ee071d3f
Arya Ashari <61337332+Aryaashari@users.noreply.github.com>
2024-06-12 08:48:23 +0700

    Update package unit test

--- src/test/java/com/example/kalkulatorsuhu/AppTest.java
+++ src/test/java/com/example/kalkulatorsuhu/AppTest.java
@@ -1,60 +1,28 @@
-
-
-import org.junit.jupiter.api.Test;
-
 import com.example.kalkulatorsuhu.KalkulatorSuhu;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppTest {
+public class AppTest {
 
     @Test
-    void testGetKelvinFromCelcius() {
+    public void testGetKelvinFromCelcius() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
         assertEquals(273, kalkulator.getKelvin());
     }
 
     @Test
-    void testGetKelvinFromKelvin() {
+    public void testGetKelvinFromKelvin() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
         assertEquals(273, kalkulator.getKelvin());
     }
 
     @Test
-    void testGetKelvinFromFahrenheit() {
+    public void testGetKelvinFromFahrenheit() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(32, "Fahrenheit");
         assertEquals(273, kalkulator.getKelvin());
     }
 
     @Test
-    void testGetCelciusFromKelvin() {
+    public void testGetCelciusFromKelvin() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
-        assertEquals(0, kalkulator.getCelcius());
-    }
-
-    @Test
-    void testGetCelciusFromFahrenheit() {
-        KalkulatorSuhu kalkulator = new KalkulatorSuhu(32, "Fahrenheit");
-        assertEquals(0, kalkulator.getCelcius());
-    }
-
-    @Test
-    void testGetFahrenheitFromCelcius() {
-        KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
-        assertEquals(32, kalkulator.getFahrenheit());
-    }
-
-    @Test
-    void testGetFahrenheitFromKelvin() {
-        KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
-        assertEquals(32, kalkulator.getFahrenheit());
-    }
-
-    @Test
-    void testChangeSuhu() {
-        KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
-        kalkulator.changeSuhu(100, "Fahrenheit");
-        assertEquals(100, kalkulator.suhu);
-        assertEquals("Fahrenheit", kalkulator.jenis_satuan_awal);
-    }
-}
+        ass
