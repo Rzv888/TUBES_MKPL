6f1280417e88349218dfbb09808ee315f84b27dc
Arya Ashari <61337332+Aryaashari@users.noreply.github.com>
2024-06-12 08:50:10 +0700

    Update AppTest.java

--- src/test/java/com/example/kalkulatorsuhu/AppTest.java
+++ src/test/java/com/example/kalkulatorsuhu/AppTest.java
@@ -25,4 +25,32 @@ public class AppTest {
     @Test
     public void testGetCelciusFromKelvin() {
         KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
-        ass
+        assertEquals(0, kalkulator.getCelcius());
+    }
+
+    @Test
+    public void testGetCelciusFromFahrenheit() {
+        KalkulatorSuhu kalkulator = new KalkulatorSuhu(32, "Fahrenheit");
+        assertEquals(0, kalkulator.getCelcius());
+    }
+
+    @Test
+    public void testGetFahrenheitFromCelcius() {
+        KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
+        assertEquals(32, kalkulator.getFahrenheit());
+    }
+
+    @Test
+    public void testGetFahrenheitFromKelvin() {
+        KalkulatorSuhu kalkulator = new KalkulatorSuhu(273, "Kelvin");
+        assertEquals(32, kalkulator.getFahrenheit());
+    }
+
+    @Test
+    public void testChangeSuhu() {
+        KalkulatorSuhu kalkulator = new KalkulatorSuhu(0, "Celcius");
+        kalkulator.changeSuhu(100, "Fahrenheit");
+        assertEquals(100, kalkulator.suhu);
+        assertEquals("Fahrenheit", kalkulator.jenis_satuan_awal);
+    }
+}
